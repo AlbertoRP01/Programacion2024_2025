1e163e16073bc4028cf4e64b451e786fb9ed3a5f
ARP01 <albertoriv.7@gmail.com>
2024-10-08 19:47:39 +0200

    Ejercicios actualizados

---
+++ workspace/Boletin1/src/Ej4_Boletin2_1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Ej4_Boletin2_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int vida = 5;
+        int num_aleatorio = (int) (Math.random() * 100) + 1;
+        do {
+            System.out.println("introduce el numero");
+            num = sc.nextInt();
+            if (num < num_aleatorio && num > 1 && num < 100) {
+                System.out.println(num + " esta por debajo ");
+
+            } else if (num > num_aleatorio && num > 1 && num < 100) {
+                System.out.println(num + " esta por encima");
+
+            } else if (num > num_aleatorio || num < 1 || num > 100) {
+
+            }
+            vida--;
+        } while (vida > 0 && num != num_aleatorio);
+        sc.close();
+        if (num == num_aleatorio) {
+            System.out.println("ganaste");
+        } else {
+            System.out.println("perdiste");
+        }
+
+
+    }
+
+}
+
