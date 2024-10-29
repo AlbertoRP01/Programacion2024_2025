68cae3033041f1ffa6e8636be82b933e034270a4
AlbertoRP01 <albertoriv.7@gmail.com>
2024-10-29 14:43:16 +0100

    subida inicial

---
+++ workspace/BoletinArrays/src/Ej12_Boletin_Array.java
@@ -0,0 +1,16 @@
+public class Ej12_Boletin_Array {
+    public static void main(String[] args) {
+        int[] array = {5, 2, 6, 3, 1, 9};
+
+    }
+    public  int[] metodoBurbuja(int[] array){
+        for (int i = 0; i< array.length -1; i --  ){
+            for (int j = 0; i > array.length +  1; i ++ ){
+                if (array[i] != 0  && array[j] != 0 ){
+
+                }
+            }
+        }
+        return array;
+    }
+}
